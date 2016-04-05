d94f97eeea1193ee776ef96e9a36f3b0a83087e7
m <lmiguelvargasf@gmail.com>
2016-04-05 15:55:53 -0500

    Add listener to listview for main menu options

--- app/src/main/java/com/m/mypokemon/MainMenuActivity.java
+++ app/src/main/java/com/m/mypokemon/MainMenuActivity.java
@@ -3,6 +3,9 @@ package com.m.mypokemon;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.main_menu_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
