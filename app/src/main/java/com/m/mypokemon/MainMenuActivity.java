9d97629f3948bedf3e7bb3e25f0d6f1f92a91be7
m <lmiguelvargasf@gmail.com>
2016-04-08 17:04:44 -0500

    Remove line

--- app/src/main/java/com/m/mypokemon/MainMenuActivity.java
+++ app/src/main/java/com/m/mypokemon/MainMenuActivity.java
@@ -25,7 +25,6 @@ public class MainMenuActivity extends AppCompatActivity {
                     intent.putExtra("position", position);
                     startActivity(intent);
                 }
-
             }
         };
 
