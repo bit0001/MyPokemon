82d4ed5954e5cf89618aa48a98d8ebb48b47e9f4
m <lmiguelvargasf@gmail.com>
2016-04-08 23:18:20 -0500

    Create SubSubMenuActivity

---
+++ app/src/main/java/com/m/mypokemon/SubSubMenuActivity.java
@@ -0,0 +1,12 @@
+package com.m.mypokemon;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class SubSubMenuActivity extends ListActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
