0f7eee09f6201bd7e28cdcd570a1e4805a306b57
m <lmiguelvargasf@gmail.com>
2016-04-11 23:59:33 -0500

    Remove ActivityName from toolbar

--- app/src/main/java/com/m/mypokemon/VideoGameActivity.java
+++ app/src/main/java/com/m/mypokemon/VideoGameActivity.java
@@ -13,8 +13,6 @@ public class VideoGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_game);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
