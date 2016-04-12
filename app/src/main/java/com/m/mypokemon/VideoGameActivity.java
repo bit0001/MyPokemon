1bf34756c729d70d1b22a0508335f5722f552554
m <lmiguelvargasf@gmail.com>
2016-04-12 01:58:52 -0500

    Assert videoGameSpinner is not null

--- app/src/main/java/com/m/mypokemon/VideoGameActivity.java
+++ app/src/main/java/com/m/mypokemon/VideoGameActivity.java
@@ -59,6 +59,7 @@ public class VideoGameActivity extends AppCompatActivity {
         }
 
         final VideoGame finalPkmVideoGame = pkmVideoGame;
+        assert videoGameSpinner != null;
         videoGameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
