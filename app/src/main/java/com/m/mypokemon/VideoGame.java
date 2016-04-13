6156b93242e0bdfd572c3388472e24e53a1a70b9
m <lmiguelvargasf@gmail.com>
2016-04-12 20:12:16 -0500

    Create video games for third generation

--- app/src/main/java/com/m/mypokemon/VideoGame.java
+++ app/src/main/java/com/m/mypokemon/VideoGame.java
@@ -7,55 +7,56 @@ import java.util.HashMap;
 public class VideoGame {
 
     public static final VideoGame RED_AND_BLUE = new VideoGame(
-                    new HashMap<String, Integer>(){
-                        {
-                            put("Red", R.drawable.red);
-                            put("Blue", R.drawable.blue);
-                        }
-                    },
-                    new HashMap<String, String>(){
-                        {
-                            put("Red", "http://www.amazon.com/Pok%C3%A9mon-Red-Version-Digital-Code/dp/B017WJY65Q/ref=sr_1_1?s=videogames&ie=UTF8&qid=1460471573&sr=1-1&keywords=pokemon+red");
-                            put("Blue", "http://www.amazon.com/Pok%C3%A9mon-Blue-Version-Digital-Code/dp/B017WJYCZA/ref=sr_1_1?s=videogames&ie=UTF8&qid=1460471669&sr=1-1&keywords=pokemon+blue");
-                        }
-                    },
-                    "Game Freak",
-                    "Game Boy and Nintendo 3DS",
-                    "Nintendo",
-                    "September 28, 1998");
+            new HashMap<String, Integer>(){
+                {
+                    put("Red", R.drawable.red);
+                    put("Blue", R.drawable.blue);
+                }
+            },
+            new HashMap<String, String>(){
+                {
+                    put("Red", "http://www.amazon.com/Pok%C3%A9mon-Red-Version-Digital-Code/dp/B017WJY65Q/ref=sr_1_1?s=videogames&ie=UTF8&qid=1460471573&sr=1-1&keywords=pokemon+red");
+                    put("Blue", "http://www.amazon.com/Pok%C3%A9mon-Blue-Version-Digital-Code/dp/B017WJYCZA/ref=sr_1_1?s=videogames&ie=UTF8&qid=1460471669&sr=1-1&keywords=pokemon+blue");
+                }
+            },
+            "Game Freak",
+            "Game Boy and Nintendo 3DS",
+            "Nintendo",
+            "September 28, 1998");
+
     public static final VideoGame YELLOW = new VideoGame(
-                    new HashMap<String, Integer>() {
-                        {
-                            put("Yellow", R.drawable.yellow);
-                        }
-                    },
-                    new HashMap<String, String>(){
-                        {
-                            put("Yellow", "http://www.amazon.com/Pok%C3%A9mon-Yellow-Version-Digital-Code/dp/B017WJY9V2/ref=sr_1_1?s=videogames&ie=UTF8&qid=1460471716&sr=1-1&keywords=pokemon+yellow");
-                        }
-                    },
-                    "Game Freak",
-                    "Game Boy and Nintendo 3DS",
-                    "Nintendo",
-                    "October 18, 1999");
+            new HashMap<String, Integer>() {
+                {
+                    put("Yellow", R.drawable.yellow);
+                }
+            },
+            new HashMap<String, String>(){
+                {
+                    put("Yellow", "http://www.amazon.com/Pok%C3%A9mon-Yellow-Version-Digital-Code/dp/B017WJY9V2/ref=sr_1_1?s=videogames&ie=UTF8&qid=1460471716&sr=1-1&keywords=pokemon+yellow");
+                }
+            },
+            "Game Freak",
+            "Game Boy and Nintendo 3DS",
+            "Nintendo",
+            "October 18, 1999");
 
     public static final VideoGame GOLD_AND_SILVER = new VideoGame(
-                    new HashMap<String, Integer>() {
-                        {
-                            put("Gold", R.drawable.gold);
-                            put("Silver", R.drawable.silver);
-                        }
-                    },
-                    new HashMap<String, String>(){
-                        {
-                            put("Gold", "http://www.amazon.com/Pokemon-Gold-Game-Boy-Color/dp/B00004TEYR/ref=sr_1_1?ie=UTF8&qid=1460499464&sr=8-1&keywords=pokemon+gold");
-                            put("Silver", "http://www.amazon.com/Pokemon-Silver-Version-Game-Boy-Color/dp/B00004TCT3/ref=sr_1_1?ie=UTF8&qid=1460499522&sr=8-1&keywords=pokemon+silver");
-                        }
-                    },
-                    "Game Freak",
-                    "Game Boy Color",
-                    "Nintendo",
-                    "October 15, 2000");
+            new HashMap<String, Integer>() {
+                {
+                    put("Gold", R.drawable.gold);
+                    put("Silver", R.drawable.silver);
+                }
+            },
+            new HashMap<String, String>(){
+                {
+                    put("Gold", "http://www.amazon.com/Pokemon-Gold-Game-Boy-Color/dp/B00004TEYR/ref=sr_1_1?ie=UTF8&qid=1460499464&sr=8-1&keywords=pokemon+gold");
+                    put("Silver", "http://www.amazon.com/Pokemon-Silver-Version-Game-Boy-Color/dp/B00004TCT3/ref=sr_1_1?ie=UTF8&qid=1460499522&sr=8-1&keywords=pokemon+silver");
+                }
+            },
+            "Game Freak",
+            "Game Boy Color",
+            "Nintendo",
+            "October 15, 2000");
 
     public static final VideoGame CRYSTAL = new VideoGame(
             new HashMap<String, Integer>() {
@@ -72,6 +73,58 @@ public class VideoGame {
             "Game Boy Color",
             "Nintendo",
             "July 30, 2001");
+
+    public static final VideoGame RUBY_AND_SAPPHIRE = new VideoGame(
+            new HashMap<String, Integer>() {
+                {
+                    put("Ruby", R.drawable.ruby);
+                    put("Sapphire", R.drawable.sapphire);
+                }
+            },
+            new HashMap<String, String>(){
+                {
+                    put("Ruby", "http://www.amazon.com/Pok%C3%A9mon-Ruby-Version-Game-Boy-Advance/dp/B000084313/ref=sr_1_2?ie=UTF8&qid=1460509065&sr=8-2&keywords=pokemon+ruby");
+                    put("Sapphire", "http://www.amazon.com/Pokemon-Sapphire-Version-Game-Boy-Advance/dp/B000084314/ref=sr_1_2?ie=UTF8&qid=1460509237&sr=8-2&keywords=pokemon+sapphire");
+                }
+            },
+            "Game Freak",
+            "Game Boy Advance",
+            "Nintendo",
+            "March 19, 2003");
+
+    public static final VideoGame FIRERED_AND_LEAFGREEN = new VideoGame(
+            new HashMap<String, Integer>() {
+                {
+                    put("FireRed", R.drawable.fire_red);
+                    put("LeafGreen", R.drawable.leaf_green);
+                }
+            },
+            new HashMap<String, String>(){
+                {
+                    put("FireRed", "http://www.amazon.com/Pokemon-FireRed-Version-Game-Boy-Advance/dp/B0006GBCZU/ref=sr_1_1?ie=UTF8&qid=1460508428&sr=8-1&keywords=firered");
+                    put("LeafGreen", "http://www.amazon.com/Pokemon-Green-Version-Game-Boy-Advance/dp/B0006GBD04/ref=sr_1_1?ie=UTF8&qid=1460508401&sr=8-1&keywords=leafgreen");
+                }
+            },
+            "Game Freak",
+            "Game Boy Advance",
+            "Nintendo",
+            "September 9, 2004");
+
+    public static final VideoGame EMERALD = new VideoGame(
+            new HashMap<String, Integer>() {
+                {
+                    put("Emerald", R.drawable.emerald);
+                }
+            },
+            new HashMap<String, String>(){
+                {
+                    put("Emerald", "http://www.amazon.com/Pokemon-Emerald-Version-Game-Boy-Advance/dp/B0007D4MVI/ref=sr_1_1?ie=UTF8&qid=1460509340&sr=8-1&keywords=pokemon+emerald");
+                }
+            },
+            "Game Freak",
+            "Game Boy Advance",
+            "Nintendo",
+            "May 1, 2005");
 
     private HashMap<String, Integer> coverIds;
     private HashMap<String, String> urls;
