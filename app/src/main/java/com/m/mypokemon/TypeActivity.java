7ab7ccfe560043118da7eef57108d8e37b76d063
m <lmiguelvargasf@gmail.com>
2016-04-14 12:30:48 -0500

    Remove activity name from toolbar

--- app/src/main/java/com/m/mypokemon/TypeActivity.java
+++ app/src/main/java/com/m/mypokemon/TypeActivity.java
@@ -13,8 +13,6 @@ public class TypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
