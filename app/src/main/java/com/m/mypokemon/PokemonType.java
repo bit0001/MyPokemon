27324c5708af4b853f2c6602e02547d729284d94
m <lmiguelvargasf@gmail.com>
2016-04-14 16:22:08 -0500

    Create PokemonType enumeration

---
+++ app/src/main/java/com/m/mypokemon/PokemonType.java
@@ -0,0 +1,6 @@
+package com.m.mypokemon;
+
+public enum PokemonType {
+    NORMAL, FIGHT, FLYING, POISON, GROUND, ROCK, BUG, GHOST, STEEL, FIRE, WATER, GRASS, ELECTRIC,
+    PSYCHIC, ICCE, DRAGON, DARK, FAIRY
+}
