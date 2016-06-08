8061e156c1fd997d8320639174f685dde6a2ada3
m <lmiguelvargasf@gmail.com>
2016-06-08 13:18:24 -0500

    Create LogIn Activity

---
+++ app/src/main/java/com/m/mypokemon/ui/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.m.mypokemon.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.m.mypokemon.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
